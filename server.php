93c0c40a5be65ccaf71579114fc5fc019122efa1
Alex Tolstov <tolstov4444@gmail.com>
2021-04-06 17:39:44 +0300

    test commit 2

--- server.php
+++ server.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-
-$app = new Comet\Comet();
+exec("hostname -I | awk '{print $1}'", $ip);
+$ip = $ip[0];
+$app = new Comet\Comet(['host' => $ip]);
 
 $app->get('/hello', 
     function ($request, $response) {              

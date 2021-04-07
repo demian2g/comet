61ab0b8534198d6231a20227cdd72e2b8b86a8e0
demian2g <demian@mail.ua>
2021-04-07 12:46:27 +0300

    gzip

--- server.php
+++ server.php
@@ -12,6 +12,7 @@ $app->get('/hello',
     function ($request, $response) use ($db) {
         $result = $db->query("SELECT TOP 10 * FROM Person.Contact")->fetchAll();
         return $response
-            ->with($result);
+            ->setHeaders([ 'Content-Type' => 'application/json; charset=utf-8', 'Content-Encoding' => 'gzip' ])
+            ->with(gzencode(json_encode($result)));
 });
 $app->run();

cc05eae12b51cd1cf4f1370d90fe3a10e25c9b29
demian2g <demian@mail.ua>
2021-04-07 15:31:28 +0300

    debug

--- server.php
+++ server.php
@@ -29,7 +29,7 @@ $app->get('/hello',
                 ->with(gzencode(json_encode($result)));
         } else {
             return $response
-                ->withText($db->errorInfo());
+                ->with($db->errorInfo());
         }
 });
 $app->run();
